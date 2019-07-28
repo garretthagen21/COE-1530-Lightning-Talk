9011f08303257c2e9519fe9c146cd5f2906c01cf
Garrett Hagen <gbh8@pitt.edu>
2019-07-28 04:06:13 -0400

    done

--- ExampleCode.swift
+++ ExampleCode.swift
@@ -89,3 +89,114 @@ class PostFeedViewController : UIViewController {
         postRef.removeObserverWithHandle(postHandle)
     }
 }
+
+
+// Local file you want to upload
+let localFile = URL(string: "path/to/image")!
+
+// Create the file metadata
+let metadata = StorageMetadata()
+metadata.contentType = "image/jpeg"
+
+// Upload file and metadata to the object 'images/mountains.jpg'
+let uploadTask = storageRef.putFile(from: localFile, metadata: metadata)
+
+// Listen for state changes, errors, and completion of the upload.
+uploadTask.observe(.resume) { snapshot in
+    // Upload resumed, also fires when the upload starts
+}
+
+uploadTask.observe(.pause) { snapshot in
+    // Upload paused
+}
+
+uploadTask.observe(.progress) { snapshot in
+    // Upload reported progress
+    let percentComplete = 100.0 * Double(snapshot.progress!.completedUnitCount)
+        / Double(snapshot.progress!.totalUnitCount)
+}
+
+uploadTask.observe(.success) { snapshot in
+    // Upload completed successfully
+}
+
+uploadTask.observe(.failure) { snapshot in
+    if let error = snapshot.error as? NSError {
+        switch (StorageErrorCode(rawValue: error.code)!) {
+        case .objectNotFound:
+            // File doesn't exist
+            break
+        case .unauthorized:
+            // User doesn't have permission to access file
+            break
+        case .cancelled:
+            // User canceled the upload
+            break
+            
+            /* ... */
+            
+        case .unknown:
+            // Unknown error occurred, inspect the server response
+            break
+        default:
+            // A separate error occurred. This is a good place to retry the upload.
+            break
+        }
+    }
+}
+
+// Create a reference to the file we want to download
+let starsRef = storageRef.child("images/stars.jpg")
+
+// Start the download (in this case writing to a file)
+let downloadTask = storageRef.write(toFile: localURL)
+
+// Observe changes in status
+downloadTask.observe(.resume) { snapshot in
+    // Download resumed, also fires when the download starts
+}
+
+downloadTask.observe(.pause) { snapshot in
+    // Download paused
+}
+
+downloadTask.observe(.progress) { snapshot in
+    // Download reported progress
+    let percentComplete = 100.0 * Double(snapshot.progress!.completedUnitCount)
+        / Double(snapshot.progress!.totalUnitCount)
+}
+
+downloadTask.observe(.success) { snapshot in
+    // Download completed successfully
+}
+
+// Errors only occur in the "Failure" case
+downloadTask.observe(.failure) { snapshot in
+    guard let errorCode = (snapshot.error as? NSError)?.code else {
+        return
+    }
+    guard let error = StorageErrorCode(rawValue: errorCode) else {
+        return
+    }
+    switch (error) {
+    case .objectNotFound:
+        // File doesn't exist
+        break
+    case .unauthorized:
+        // User doesn't have permission to access file
+        break
+    case .cancelled:
+        // User cancelled the download
+        break
+        
+        /* ... */
+        
+    case .unknown:
+        // Unknown error occurred, inspect the server response
+        break
+    default:
+        // Another error occurred. This is a good place to retry the download.
+        break
+    }
+}
+
