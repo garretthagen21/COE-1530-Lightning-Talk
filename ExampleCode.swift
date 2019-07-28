87dc3cb062cad0d3b0ebf1f03b654405166c59cf
Garrett Hagen <gbh8@pitt.edu>
2019-07-28 02:24:39 -0400

    database

--- ExampleCode.swift
+++ ExampleCode.swift
@@ -34,4 +34,58 @@ authHandle = Auth.auth().addStateDidChangeListener{ auth, user in
     }
 }
 
+// Create the database reference
+let dbRef = Database.database().reference()
 
+// Generate a unique key for the new post
+guard let key = ref.child("posts").childByAutoId().key else { return }
+
+// Create the new post
+let post = ["uid": userID,
+            "author": username,
+            "title": title,
+            "body": body]
+
+// Specify the paths at where this post will be written
+let childUpdates = ["/posts/\(key)": post,
+                    "/user-posts/\(userID)/\(key)/": post]
+
+// Perform the updates
+dbRef.updateChildValues(childUpdates)
+
+
+
+
+
+
+class PostFeedViewController : UIViewController {
+    var postRef: DatbaseReference!
+    var postHandle: DatabaseHandle!
+    
+    // Called once when the view is initialized
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initialize the database reference
+       dbRef = Database.database().reference.child("posts")
+    }
+    
+    // Called every time the view appears
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Attach the listener to our post reference
+        postHandle = postRef.observe(DataEventType.value, with: { (snapshot) in
+            let postDict = snapshot.value as? [String : AnyObject] ?? [:]
+            // Extract the contents of postDict and reload the UI
+        })
+    }
+    
+    // Called every time the view dissapears
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        // Detach the listener to our post reference
+        postRef.removeObserverWithHandle(postHandle)
+    }
+}
